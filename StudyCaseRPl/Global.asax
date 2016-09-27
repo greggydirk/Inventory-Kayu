8a78d0b0df3516da02bd4f8650a6c5b9602c5641
HIDAYAT <HIDAYAT@HIDAYAT-PC>
2016-09-27 14:14:40 +0700

    Add project files.

---
+++ StudyCaseRPl/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudyCaseRPl.MvcApplication" Language="C#" %>
---
+++ StudyCaseRPl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudyCaseRPl.Startup))]
+namespace StudyCaseRPl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
