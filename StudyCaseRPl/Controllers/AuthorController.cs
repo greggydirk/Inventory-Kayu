e481b6cfff3f0610bc9388b35532832adcff0e84
HIDAYAT <HIDAYAT@HIDAYAT-PC>
2016-09-27 17:02:42 +0700

    Membuat AuthorsDAL

---
+++ StudyCaseRPl/Controllers/AuthorController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudyCaseRPl.DAL;
+using StudyCaseRPl.Models;
+using StudyCaseRPl.Controllers;
+namespace StudyCaseRPl.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        public ActionResult Index()
+        {
+            AuthorDAL autdal = new AuthorDAL();
+            var models = autdal.GetAll();
+            return View(models);
+            
+        }
+
+        // GET: Author/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Author/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Author/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Author/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Author/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Author/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Author/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ StudyCaseRPl/DAL/AuthorDAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StudyCaseRPl.Models;
+
+namespace StudyCaseRPl.DAL
+{
+    public class AuthorDAL
+    {
+        private SampleShopDBEntities db;
+        public AuthorDAL()
+        {
+            db = new SampleShopDBEntities();
+        }
+        //Menampilkan semua data author
+        public IQueryable<Author> GetAll()
+        {
+            var result = from c in db.Authors
+                         orderby c.FisrtName ascending
+                         select c;
+
+            return result;
+        }
+        public Author GetById(int id)
+        {
+            var result = (from c in db.Authors
+                          where c.AuthorID == id
+                          select c).FirstOrDefault();
+            if (result != null)
+                return result;
+            else
+                throw new Exception("Data dengan id" + id.ToString() + "TIdak Ditemukan");
+        }
+    }
+}
